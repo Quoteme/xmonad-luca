b34300a9437640cce3d2e17184886d21e58caca4
Luca Leon Happel <lucahappel99@gmx.de>
2023-06-12 17:31:30 +0200

    add: window manager minimization support
    this allows xfce4-panel for example to minimize and restore windows

--- src/Hooks.hs
+++ src/Hooks.hs
@@ -30,6 +30,7 @@ import Thumbnail
 import Text.Format
 import Keybindings
 import LaptopMode
+import XMonad.Hooks.Minimize (minimizeEventHook)
 
 -- {{{ The client events that xmonad is interested in
 myClientMask = focusChangeMask .|. clientMask def
@@ -50,6 +51,7 @@ myManageHook = composeAll
 
 -- {{{ Event hook
 myEventHook = focusOnMouseMove
+            <+> minimizeEventHook
             <+> hintsEventHook
             <+> windowedFullscreenFixEventHook
             <+> notificationsOnTop
