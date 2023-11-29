5a7853b2bd2646a92a27978667f41a1ccca7de73
Luca Leon Happel <lucahappel99@gmx.de>
2023-11-29 22:46:57 +0100

    add: some more custom layout instances

--- src/Layouts/TreeLayout.hs
+++ src/Layouts/TreeLayout.hs
@@ -30,7 +30,7 @@ import XMonad.StackSet
 -- | [window3]
 -- | [window4]
 data Axis = RIGHT | LEFT | UP | DOWN
-  deriving (Show, Read)
+  deriving (Show, Read, Eq)
 
 -- | Container to store all the extra-data for a node in our [TreeLayout].
 data WindowNode a = WindowNode
@@ -41,20 +41,48 @@ data WindowNode a = WindowNode
   }
   deriving (Show, Read)
 
+-- | We define two [WindowNode]s to be equal if their [value]s are equal.
+-- |
+-- | In practice, this means we would consider two [WindowNode Window]s to be
+-- | equal if they store the same window.
+-- |
+-- | We consider two [WindowNode (Tree Window)]s to be equal if they store the same
+instance Eq a => Eq (WindowNode a) where
+  (==) :: WindowNode a -> WindowNode a -> Bool
+  (==) (WindowNode _ _ _ v1) (WindowNode _ _ _ v2) = v1 == v2
+
+instance Functor WindowNode where
+  fmap :: (a -> b) -> WindowNode a -> WindowNode b
+  fmap f (WindowNode w h r v) = WindowNode w h r (f v)
+
+instance Foldable WindowNode where
+  foldr :: (a -> b -> b) -> b -> WindowNode a -> b
+  foldr f z (WindowNode _ _ _ v) = f v z
+
 -- | A datatype to represent a tree layout of windows for XMonad.
-data TreeLayout a = TreeLayout
+newtype TreeLayout a = TreeLayout
   { tree :: Tree (WindowNode a)
   }
-  deriving (Show, Read)
+  deriving (Show, Read, Eq)
+
+-- | Functor instance for our [TreeLayout].
+instance Functor TreeLayout where
+  fmap :: (a -> b) -> TreeLayout a -> TreeLayout b
+  fmap f (TreeLayout tree) = TreeLayout $ fmap (fmap f) tree
+
+instance Foldable TreeLayout where
+  foldr :: (a -> b -> b) -> b -> TreeLayout a -> b
+  foldr f z (TreeLayout tree) = foldr (flip (foldr f)) z tree
 
 instance LayoutClass TreeLayout Window where
   -- \| The name we give to our layout.
   description _ = "TreeLayout"
 
   -- \| This function will place the windows on the screen (some given [rect]).
-  pureLayout (TreeLayout tree) rect stack = [(w, rect) | w <- [XMonad.StackSet.focus stack] ++ up stack ++ down stack]
+  doLayout (TreeLayout tree) rect stack = do
+    return (l, Just (TreeLayout tree))
     where
-      a = 1
+      l = [(w, rect) | w <- [XMonad.StackSet.focus stack] ++ up stack ++ down stack]
 
 -- | This is the starting point of our layout.
 -- | You want to include this in your [layoutHook].
@@ -65,3 +93,7 @@ emptyTreeLayout =
     }
   where
     emptyTree = Node (WindowNode 0 0 RIGHT 0) []
+
+-- | Check if an element is in our TreeLayout.
+check :: Eq a => a -> TreeLayout a -> Bool
+check x (TreeLayout tree) = WindowNode x `elem` tree
