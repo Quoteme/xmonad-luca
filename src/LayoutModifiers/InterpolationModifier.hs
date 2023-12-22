c99ead193624c59823b6cf4d2316e907a86466f6
Luca Leon Happel <luca.happel@hhu.de>
2023-12-22 04:51:29 +0100

    add: interpolation modifier

--- src/LayoutModifiers/InterpolationModifier.hs
+++ src/LayoutModifiers/InterpolationModifier.hs
@@ -6,6 +6,8 @@ import Data.Maybe
 import Control.Concurrent
 import Control.Monad.Primitive (unsafePrimToIO)
 import XMonad.Layout.Decoration
+import Control.Monad
+import XMonad.StackSet (currentTag)
 
 data InterpolationModifier a = InterpolationModifier
   { -- | The interpolation function
@@ -14,64 +16,64 @@ data InterpolationModifier a = InterpolationModifier
     --   The third argument is the rectangle at the end
     --   The result is the rectangle at the current time
     interpolationFunction :: Rational -> Rectangle -> Rectangle -> Rectangle
-    -- | The time it takes to interpolate from the start to the end in seconds
+    -- | The initial positions of the windows
+    , initialPositions :: [(a, Rectangle)]
+    -- | The time it takes to interpolate from the start to the end in milliseconds
     , interpolationTime :: Rational
-    -- | The positions of all rectangles before they were moved.
-    --   We need to store this information, because we only know the positions of the rectangles
-    --   after they have been moved otherwise. The previous positions are needed to interpolate
-    --   between the old and the new positions.
-    --   Once the interpolation is finished, we update this field.
-    , lastPositions :: [(a, Rectangle)]
-    -- | The time at which we last updated the lastPositions field
-    , lastUpdate :: Maybe POSIXTime
+    -- | Number of frames to draw
+    , fps :: Int
   }
 
 instance Show a => Show (InterpolationModifier a) where
-  show (InterpolationModifier _ t l _) = "InterpolationModifier " ++ show t ++ " " ++ show l
+  show (InterpolationModifier _ _ dt fps) = "InterpolationModifier " ++ show dt ++ " " ++ show fps
 
 instance Read a => Read (InterpolationModifier a) where
-  readsPrec _ s = [(InterpolationModifier defaultInterpolationFunction 0 [] Nothing, "")]
+  readsPrec _ s = [(InterpolationModifier defaultInterpolationFunction [] 3000 10, "")]
 
-instance (Read a, Show a, Eq a) => LayoutModifier InterpolationModifier a where
-  modifierDescription (InterpolationModifier _ t _ _) = "Interpolated " ++ show t
-  redoLayout (InterpolationModifier f dt lastpos startTime) rect possibleStack wrs = do
+instance LayoutModifier InterpolationModifier Window where
+  modifierDescription _ = "Interpolated"
+
+  redoLayout (InterpolationModifier f ip dt fps) rect possibleStack wrs = do
     -- get the current time
     time <- io getPOSIXTime
-    -- if no `lastUpdate` has been set, set it to the current time and we are done
-    if isNothing startTime
-      then return (wrs, Just $ InterpolationModifier f dt lastpos (Just time))
-      else do
-        -- Otherwise, we need to interpolate the positions of the rectangles
-        -- 1. Get the percentage we are done with interpolating
-        let timePassed = time - fromJust startTime
-        let percentage = realToFrac timePassed / realToFrac dt
-        if percentage >= 1
-          -- If we are done, we can just return the new positions and reset the `lastPositions` field
-          then return (wrs, Just $ InterpolationModifier f dt [] Nothing)
-          else do
-            -- 2. For any window which is not in our `lastPositions` list, we can 
-            --    just keep its new position
-            let newWindows = [(w, r) | (w, r) <- wrs, w `notElem` map fst lastpos]
-            -- 3. For any window which is in our `lastPositions` list, we need to
-            --    interpolate between the old and the new position
-            let oldWindows = [(w, r) | (w, r) <- wrs, w `elem` map fst lastpos]
-            let interpolatedWindows = [ (w, f percentage r1 r2) | (w, r1) <- oldWindows, (w2, r2) <- lastpos, w == w2 ]
-            -- 4. Start a new thread that will sleep for 1/10th of a second and then call `redoLayout` again
-            --    This is necessary, because we need to update the positions of the windows like an animation.
-            liftIO . forkIO $ do
-              threadDelay 100000
-              -- liftIO $ redoLayout (InterpolationModifier f dt interpolatedWindows (Just time)) _ _ wrs
-              -- unsafePrimToIO $ redoLayout (InterpolationModifier f dt interpolatedWindows (Just time)) rect possibleStack wrs
-              return ()
-            return (newWindows ++ interpolatedWindows, Just $ InterpolationModifier f dt lastpos (Just time))
+    -- calculate the number of frame we need to draw
+    let frames = ceiling $ dt * fi fps / 1000
+    -- calculate the time between two frames
+    let frameTime = dt / fromIntegral frames
+    -- calculate in how many milliseconds we need to draw which frame
+    let percentages = [fi i / fi frames | i <- [0..frames]]
+    -- position the windows into their new rectangles (using X11 functions).
+    -- Then wait i*frameTime milliseconds and repeat.
+    io $ do
+      dpy <- openDisplay ""
+      forM_ percentages $ \percentage -> do
+        -- wait until the next frame
+        threadDelay $ round frameTime
+        -- for each window for which we saved its initial position and for which we have a new position
+        -- interpolate between the two positions and move the window to the new position
+        let interpolatings = [(a,r1,r2) | (a,r1) <- ip, Just r2 <- [lookup a wrs]]
+        -- transform each `a` into a window id
+        forM_ interpolatings $ \(a,r1,r2) -> do
+          let r = f percentage r1 r2
+          -- first move the window to the upper left corner using `moveWindow`
+          moveWindow dpy a (rect_x r) (rect_y r)
+          -- then resize the window using `resizeWindow`
+          resizeWindow dpy a (rect_width r) (rect_height r)
+      closeDisplay dpy
+    return (wrs, Just $ InterpolationModifier f wrs dt fps)
 
 defaultInterpolationFunction :: Rational -> Rectangle -> Rectangle -> Rectangle
 defaultInterpolationFunction percentage (Rectangle x1 y1 w1 h1) (Rectangle x2 y2 w2 h2) =
       Rectangle (x1 + fi (round $ percentage * fi (x2 - x1))) (y1 + fi (round $ percentage * fi (y2 - y1))) (w1 + fi (round $ percentage * fi (w2 - w1))) (h1 + fi (round $ percentage * fi (h2 - h1)))
 
-animate :: l a -> InterpolationModifier a
-animate l = InterpolationModifier { interpolationFunction = defaultInterpolationFunction
-                                  , interpolationTime = 0
-                                  , lastPositions = []
-                                  , lastUpdate = Nothing
-                                  }
+
+defaultInterpolationModifier :: InterpolationModifier a
+defaultInterpolationModifier = InterpolationModifier {
+  interpolationFunction = defaultInterpolationFunction
+  , interpolationTime = 3000
+  , initialPositions = []
+  , fps = 10
+  } 
+
+animate :: l a -> ModifiedLayout InterpolationModifier l a
+animate = ModifiedLayout defaultInterpolationModifier
