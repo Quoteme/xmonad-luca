5b703156918cec7669c428f16f091c3f1ac9473a
Luca Leon Happel <lucahappel99@gmx.de>
2024-08-06 01:14:31 +0200

    add: function to signal a layout change over DBUS

--- src/DBusServer.hs
+++ src/DBusServer.hs
@@ -6,10 +6,8 @@ import Control.Monad
 import Control.Monad.IO.Class
 import DBus
 import DBus.Client
+import DBus.Introspection (Signal (Signal, signalArgs, signalName))
 import State qualified
-
-sayHello :: String -> IO String
-sayHello name = return ("Hello " ++ name ++ "!")
 
 start :: TVar State.AppState -> IO ()
 start appState = do
@@ -29,7 +27,7 @@ start appState = do
     client
     (objectPath_ "/general")
     defaultInterface
-      { interfaceName = (interfaceName_ "org.xmonad.bus")
+      { interfaceName = interfaceName_ "org.xmonad.bus"
       , interfaceMethods =
           [ autoMethod (memberName_ "Layout") $ do
               state <- readTVarIO appState
@@ -37,12 +35,38 @@ start appState = do
           , autoMethod (memberName_ "Layouts") $ do
               state <- readTVarIO appState
               return (State.layouts state)
+          , autoMethod (memberName_ "Workspace") $ do
+              state <- readTVarIO appState
+              return (State.workspace state)
           , autoMethod (memberName_ "Workspaces") $ do
               state <- readTVarIO appState
               return (State.workspaces state)
-          , autoMethod (memberName_ "Hello") sayHello
+          ]
+      , interfaceSignals =
+          [ Signal
+              { signalName = memberName_ "LayoutChanged"
+              , signalArgs = []
+              }
+          , Signal
+              { signalName = memberName_ "WorkspacesChanged"
+              , signalArgs = []
+              }
+          , Signal
+              { signalName = memberName_ "WorkspaceChanged"
+              , signalArgs = []
+              }
           ]
       }
-
   -- wait forever for calls
   forever (threadDelay 1000000)
+
+signalLayoutChanged :: Client -> IO ()
+signalLayoutChanged client = do
+  emit client $
+    ( signal
+        (objectPath_ "/general")
+        (interfaceName_ "org.xmonad.bus")
+        (memberName_ "LayoutChanged")
+    )
+      { signalBody = [toVariant "hallo"]
+      }
